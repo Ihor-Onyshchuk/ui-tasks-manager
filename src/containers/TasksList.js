73e3581c9ed90a4de9964f7649fdfb49854f9a6b
Ihor-Onyshchuk <onyshchukihor11@gmail.com>
2020-04-16 17:41:09 +0300

    added task filter

--- src/containers/TasksList.js
+++ src/containers/TasksList.js
@@ -9,10 +9,14 @@ export class TasksList extends PureComponent {
   state = {
     tasks: [],
     isLoading: true,
+    activeFilter: {
+      name: "dueBy",
+      value: "desc",
+    },
   };
 
   componentDidMount() {
-    instance.get("tasks").then((res) => {
+    instance.get("tasks?sort=dueBy%20desc").then((res) => {
       const tasks = res.data.tasks.map((task) => ({
         ...task,
         dueBy: dateFormatter(task.dueBy * 1000, "dd mmmm, yyyy"),
@@ -30,8 +34,51 @@ export class TasksList extends PureComponent {
     });
   };
 
+  handleFilterChange = (name, value) => {
+    instance.get(`tasks?sort=${name}%20${value}`).then((res) => {
+      const tasks = res.data.tasks.map((task) => ({
+        ...task,
+        dueBy: dateFormatter(task.dueBy * 1000, "dd mmmm, yyyy"),
+      }));
+      this.setState({
+        tasks: tasks,
+        isLoading: false,
+        activeFilter: { name: name, value: value },
+      });
+    });
+  };
+
   render() {
     if (this.state.isLoading) return <Preloader />;
+    const { value, name } = this.state.activeFilter;
+
+    const filterMapping = {
+      title: { name: "title", value: "asc" },
+      dueBy: { name: "dueBy", value: "asc" },
+      priority: { name: "priority", value: "asc" },
+    };
+
+    const renderRow = (task) => {
+      const { title, dueBy, priority, id } = task;
+      return (
+        <tr key={id}>
+          <td>
+            <NavLink to={`${this.props.match.path}/${id}`}>{title}</NavLink>
+          </td>
+          <td>{dueBy}</td>
+          <td>{priority}</td>
+          <td>
+            <NavLink to={`${this.props.match.path}/${id}/update`}>
+              <i className="fas fa-pencil-alt mr-3"></i>
+            </NavLink>
+            <i
+              onClick={() => this.handleDelete(id)}
+              className="far fa-trash-alt"
+            ></i>
+          </td>
+        </tr>
+      );
+    };
 
     return (
       <div>
@@ -39,26 +86,55 @@ export class TasksList extends PureComponent {
         {!this.state.tasks.length ? (
           <div>Its no tasks yet! Please press to button below to add task!</div>
         ) : (
-          this.state.tasks.map((task) => (
-            <ul key={task.id} className="tasks">
-              <li className="task">
-                <NavLink to={`${this.props.match.path}/${task.id}`}>
-                  <strong>title: </strong>
-                  {task.title}
-                </NavLink>
-                <span>
-                  <strong>date:</strong> {task.dueBy}
-                </span>{" "}
-                <span>
-                  <strong>priority:</strong> {task.priority}
-                </span>
-                <i
-                  onClick={() => this.handleDelete(task.id)}
-                  className="far fa-trash-alt"
-                ></i>
-              </li>
-            </ul>
-          ))
+          <table className="table">
+            <thead>
+              <tr>
+                <th>
+                  <i
+                    className={`fas fa-chevron-${
+                      value === "desc" && name === "title" ? "down" : "up"
+                    }`}
+                    onClick={() =>
+                      this.handleFilterChange(
+                        "title",
+                        value === "desc" ? "asc" : "desc"
+                      )
+                    }
+                  ></i>
+                  Title
+                </th>
+                <th>
+                  <i
+                    className={`fas fa-chevron-${
+                      value === "desc" && name === "dueBy" ? "down" : "up"
+                    }`}
+                    onClick={() =>
+                      this.handleFilterChange(
+                        "dueBy",
+                        value === "desc" ? "asc" : "desc"
+                      )
+                    }
+                  ></i>
+                  Date
+                </th>
+                <th>
+                  <i
+                    className={`fas fa-chevron-${
+                      value === "desc" && name === "priority" ? "down" : "up"
+                    }`}
+                    onClick={() =>
+                      this.handleFilterChange(
+                        "priority",
+                        value === "desc" ? "asc" : "desc"
+                      )
+                    }
+                  ></i>
+                  Priority
+                </th>
+              </tr>
+            </thead>
+            <tbody>{this.state.tasks.map(renderRow)}</tbody>
+          </table>
         )}
         <NavLink to="/create-task">Add Task</NavLink>
       </div>
