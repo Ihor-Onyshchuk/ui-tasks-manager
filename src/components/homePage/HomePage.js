fcd781b180bb2f794ab69775ce8c6623460f8d7d
ihor <onyshchukihor11@gmail.com>
2020-09-05 10:16:55 +0300

    polishing

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './HomePage.scss';
 
 const HomePage = () => {
-  return (
-    <div className="home container">
-      <div className="row">
-        <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-          <h2 className="font-weight-bold pb-2 mt-5 alert display-4">
-            Welcome to Groovy tasks manager!
-          </h2>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='home container'>
+			<div className='row'>
+				<div className='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12'>
+					<h2 className='font-weight-bold pb-2 mt-5 alert display-4'>
+						Welcome to Groovy tasks manager!
+					</h2>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
--- src/containers/calendar/Calendar.js
+++ src/containers/calendar/Calendar.js
@@ -1,76 +1,76 @@
-import React, { PureComponent, Fragment } from "react";
-import "./Calendar.scss";
-import { monthFullName, dayWeekFullName } from "../../utils/date/static";
+import React, { PureComponent, Fragment } from 'react';
+import './Calendar.scss';
+import { monthFullName, dayWeekFullName } from '../../utils/date/static';
 
 export default class Calendar extends PureComponent {
-  state = {
-    year: new Date().getFullYear(),
-    month: new Date().getMonth(),
-    dayInCurrentMonth: new Date(2020, 4, 0).getDate(),
-    currentDay: dayWeekFullName[new Date().getDay()],
-  };
+	state = {
+		year: new Date().getFullYear(),
+		month: new Date().getMonth(),
+		dayInCurrentMonth: new Date(2020, 4, 0).getDate(),
+		currentDay: dayWeekFullName[new Date().getDay()],
+	};
 
-  handleSelectMonth = (event) => {
-    this.setState({
-      month: +event.target.value,
-    });
-  };
+	handleSelectMonth = event => {
+		this.setState({
+			month: +event.target.value,
+		});
+	};
 
-  toggleMonth = (direction) => {
-    const monthCounter =
-      this.state.month >= 11
-        ? 0
-        : this.state.month === 0
-        ? 11
-        : this.state.month;
+	toggleMonth = direction => {
+		const monthCounter =
+			this.state.month >= 11
+				? 0
+				: this.state.month === 0
+				? 11
+				: this.state.month;
 
-    if (this.state.month >= 11 && direction > 0) {
-      this.handleYearChange(direction, monthCounter);
-    } else if (this.state.month === 0 && direction < 0) {
-      this.handleYearChange(direction, monthCounter);
-    }
+		if (this.state.month >= 11 && direction > 0) {
+			this.handleYearChange(direction, monthCounter);
+		} else if (this.state.month === 0 && direction < 0) {
+			this.handleYearChange(direction, monthCounter);
+		}
 
-    this.setState((state) => ({
-      month: state.month + direction,
-    }));
-  };
+		this.setState(state => ({
+			month: state.month + direction,
+		}));
+	};
 
-  handleYearChange = (direction, monthCounter) => {
-    this.setState((state) => ({
-      year: state.year + direction,
-      month: monthCounter,
-    }));
-  };
+	handleYearChange = (direction, monthCounter) => {
+		this.setState(state => ({
+			year: state.year + direction,
+			month: monthCounter,
+		}));
+	};
 
-  render() {
-    const { year, month, dayInCurrentMonth, currentDay } = this.state;
-    return (
-      <div>
-        <div className="d-flex justify-content-around align-items-center">
-          <i
-            className="fas fa-chevron-left"
-            onClick={() => this.toggleMonth(-1)}
-          />
-          <label className="mb-0">
-            <select value={month} onChange={this.handleSelectMonth}>
-              {monthFullName.map((month, index) => (
-                <option value={index}>{month}</option>
-              ))}
-            </select>
-          </label>
-          <div>{year}</div>
-          <i
-            className="fas fa-chevron-right"
-            onClick={() => this.toggleMonth(1)}
-          />
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const { year, month, dayInCurrentMonth, currentDay } = this.state;
+		return (
+			<div>
+				<div className='d-flex justify-content-around align-items-center'>
+					<i
+						className='fas fa-chevron-left'
+						onClick={() => this.toggleMonth(-1)}
+					/>
+					<label className='mb-0'>
+						<select value={month} onChange={this.handleSelectMonth}>
+							{monthFullName.map((month, index) => (
+								<option value={index}>{month}</option>
+							))}
+						</select>
+					</label>
+					<div>{year}</div>
+					<i
+						className='fas fa-chevron-right'
+						onClick={() => this.toggleMonth(1)}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 {
-  /* <label>
+	/* <label>
   Pick your favorite flavor:
   <select value={this.state.value} onChange={this.handleChange}>
     {" "}
